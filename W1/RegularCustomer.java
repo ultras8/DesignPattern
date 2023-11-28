352cc54e54780ca9eff996cd9b4a8e28c9f29385
Pickopiw <48793730+ultras8@users.noreply.github.com>
2023-11-28 20:10:42 +0700

    Delete W1/RegularCustomer.java

--- W1/RegularCustomer.java
+++
@@ -1,9 +0,0 @@
-//make this class a subclass of customer
-public class RegularCustomer extends Customer {
-
-    @Override
-    public String createMail() {
-        return "Regular Customer";
-    }
-    
-}
