855255449513f6d3e825d5a340fc7889c9c2a08b
Pickopiw <48793730+ultras8@users.noreply.github.com>
2023-11-28 20:10:49 +0700

    Delete W1/MountainCustomer.java

--- W1/MountainCustomer.java
+++
@@ -1,9 +0,0 @@
-//make this class a subclass of customer
-public class MountainCustomer extends Customer {
-
-    @Override
-    public String createMail(){
-        return "Mountain Customer";
-    }
-
-}
